94fdebc08bc55f0a9b607ab1b4c0bdbfbcb79652
Mercedes <meegido@gmail.com>
2020-06-28 14:06:11 +0200

    starting from scratch

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  pathPrefix: "/mecha",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mecha's blog`,
+    description: `Kick off your ass`,
+    author: `@meegido`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +25,6 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
